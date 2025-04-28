4a881f9beb8630862cb4efbc3185e656c0319d2d
JordenVS <jordenjessevs@gmail.com>
2025-04-28 11:29:25 +0200

    Added queries csv file

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ embedding = OllamaEmbeddings(model="nomic-embed-text")
 
 model = OllamaLLM(model = "llama3.2")
 
-retriever = createRetriever()
-#vectorstore = Chroma(persist_directory="chroma_db", embedding_function=embedding)
-#retriever = vectorstore.as_retriever()
+#retriever = createRetriever()
+vectorstore = Chroma(persist_directory="chroma_db_txt", embedding_function=embedding)
+retriever = vectorstore.as_retriever()
 # chroma_db is the database created by setting markdown_format to true, chunking with RecursiveCharacterTextSplitter, chunk size 500, overlap 100 with separators["\n\n", "\n", " ", ""] and nomic-embed-text as embedding.
-
+# chroma_db_text is the database created by setting markdown_format to false, chunking with RecursiveCharacterTextSplitter, chunk size 500, overlap 100 and nomic-embed-text as embedding.
 
 qa_chain = RetrievalQA.from_chain_type(llm=model, retriever=retriever)
 
-response = qa_chain.run("What can you tell me about plain literal enrichment and how it worked before?")
+response = qa_chain.run("What did the dutch state regarding article 14?")
 print(response)
