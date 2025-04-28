1d3676a4b1f927e19ec2fa7a319ca68eef9c0ac9
JordenVS <jordenjessevs@gmail.com>
2025-04-28 10:06:11 +0200

    Fix

--- vector.py
+++ vector.py
@@ -3,20 +3,27 @@ from langchain.vectorstores import Chroma
 from langchain_core.documents import Document
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import ConfluenceLoader
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from the .env file
+load_dotenv()
+
+# Retrieve the API token (or other environment variables)
+api_token = os.getenv("CONFLUENCE_API_TOKEN")
+
 import pytesseract
 
 import reportlab.graphics.renderPM as renderPM
 renderPM._renderPMBackend = "bitmap"
 
-# Confluence API token, TODO: Make modifiable
-CONFLUENCE_API_TOKEN = "ATATT3xFfGF0biwfWNN6mqTtJ2Lp1ZipEjeH5IDpE6WN8Xes5rJgFzMggQka09sy8PAp6t_9P6QXHQtrf4eili8mjqENmD2Do4oGvBAG1UrZb5_9R-Qv-UXtxoQE8e9JCcowMN2YgCv9EuayYCiOqhFZJGyI_5m5utQJEm1hJFm7v5c06JnN2zE=BFAE4785"
 
 def createRetriever():
     loader = ConfluenceLoader(
         url="https://europeana.atlassian.net/wiki/",
         username="jordenjessevs@gmail.com", 
         keep_markdown_format= True,
-        api_key=CONFLUENCE_API_TOKEN, cloud= True, space_key="EF", include_attachments=True)
+        api_key=api_token, cloud= True, space_key="EF", include_attachments=True)
 
     # Necessary for ConfluenceLoader? 
     pytesseract.pytesseract.tesseract_cmd = "C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
